bfb202b110003a54e47808d15852748aa0443fb8
Giovanni Senatore <giovanni.senatore@gmail.com>
2024-05-19 11:38:04 +0200

    Update to aggregate3.0.py
     * Add option to select to have or not geojson data of each project
     * Code style

--- aggregator3.0.py
+++ aggregator3.0.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
-import os
+# import os
 import sys
 import argparse
-import csv
+# import csv
 import json
 import re
 import pandas as pd
 import datetime as dt
-import shapely
-from shapely.geometry import shape
-from shapely.ops import transform
-import pyproj.transformer
-import pathlib
+# import shapely
+# from shapely.geometry import shape
+# from shapely.ops import transform
+# import pyproj.transformer
+# import pathlib
 import logging
 import locale
-import unicodedata#from io import StringIO
+import unicodedata
+# from io import StringIO
+# import copy
 from tqdm import tqdm
 
 
@@ -23,8 +25,8 @@ locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 logger = logging.getLogger('china-osn-data')
 
-SPHERICAL_CRS="EPSG:4326"
-PLANAR_CRS="EPSG:6893"
+SPHERICAL_CRS = "EPSG:4326"
+PLANAR_CRS = "EPSG:6893"
 GEOJSON_PATH = 'latest/geojsons'
 GEOJSON_FILE_EXTENSION = '*.geojson'
 GEOPANDAS_INDEX_COLUMN = 'id'
@@ -39,8 +41,10 @@ loglevel_defs = {
     "fatal": logging.FATAL
 }
 
+
 def to_loglevel(x):
     return loglevel_defs[x]
+
 
 header_parser_rules = [
     (re.compile(r'[()]').sub, r''),
@@ -100,7 +104,7 @@ gcdf30_headers = {
     "Source Quality Score": "float64",
     "Data Completeness Score": "float64",
     "Implementation Detail Score": "float64",
-    "Loan Detail Score": "float64",    
+    "Loan Detail Score": "float64",
 }
 
 gcdf30_datetime_columns = [
@@ -119,13 +123,16 @@ gcdf30_adm_headers = {
     "centroid_latitude": "float64",
 }
 
+
 def to_country(x):
     if isinstance(x, str):
-        return countryconvertion[x] if x in countryconvertion else unicodedata.normalize('NFD', x).encode("ascii", "ignore").decode()
+        return countryconvertion[x] if x in countryconvertion else \
+            unicodedata.normalize('NFD', x).encode("ascii", "ignore").decode()
 
 
 def to_year(x):
-    return dt.datetime.strptime(str(int(float(x))), '%Y') if x else float('NaN')
+    return dt.datetime.strptime(str(int(
+        float(x))), '%Y') if x else float('NaN')
 
 
 def to_datetime(x):
@@ -148,6 +155,7 @@ def to_bool(x):
 def to_json(o):
     if isinstance(o, dt.datetime):
         return dt.datetime.strftime(o, "%Y") if not pd.isnull(o) else None
+
 
 gcdf30_converters = {
     "Recommended For Aggregates": to_bool,
@@ -226,6 +234,7 @@ gcdf30_converters = {
     "Geospatial Feature Available": to_bool,
 }
 
+
 def date_parser(date):
     return dt.datetime.fromisoformat(date) if date else float('NaN')
 
@@ -234,7 +243,8 @@ countryconvertion = {
     "Russia": "Russian Federation",
     "Iran": "Iran (Islamic Republic of)",
     "Micronesia": "Micronesia (Federated States of)",
-    "Democratic People's Republic of Korea": "Korea (Democratic People's Republic of)",
+    "Democratic People's Republic of Korea": "Korea (Democratic People's "
+    "Republic of)",
     "Bolivia": "Bolivia (Plurinational State of)",
     "Democratic Republic of the Congo": "Congo, Democratic Republic of the",
     "Venezuela": "Venezuela (Bolivarian Republic of)",
@@ -243,7 +253,7 @@ countryconvertion = {
     "Brunei": "Brunei Darussalam",
     "Syria": "Syrian Arab Republic",
     "North Korea": "Korea (Democratic People's Republic of)",
-    "Vietnam" :"Viet Nam",
+    "Vietnam": "Viet Nam",
     "DR Congo": "Congo, Democratic Republic of the",
     "Curaçao": "Curacao",
     "Moldova": "Moldova, Republic of",
@@ -255,13 +265,15 @@ countryconvertion = {
     "Multi-Region": float('NaN'),
 }
 
+
 class CountryCode():
     def __init__(self, filename):
         with open(filename, 'r') as f:
             self._data = json.load(f)
         for c in self._data:
-            c["name"] = unicodedata.normalize('NFD', c["name"]).encode("ascii", "ignore").decode()
-            #print(c["name"])
+            c["name"] = unicodedata.normalize(
+                'NFD', c["name"]).encode("ascii", "ignore").decode()
+            # print(c["name"])
 
     def lookup(self, x):
         if pd.isna(x):
@@ -269,33 +281,53 @@ class CountryCode():
         for c in self._data:
             if c["alpha-3"] == x:
                 return c["name"]
-        #raise KeyError("{x} not found".format(x=x))
+        # raise KeyError("{x} not found".format(x=x))
         return float('NaN')
 
     def reverse_lookup(self, x):
         if pd.isna(x):
             return [float('NaN')]
         for c in self._data:
-            if c["name"] == unicodedata.normalize('NFD', x).encode("ascii", "ignore").decode():
+            if c["name"] == unicodedata.normalize(
+                    'NFD', x).encode("ascii", "ignore").decode():
                 return c["alpha-3"]
         raise KeyError("{x} not found".format(x=x))
-        #return float('NaN')
+        # return float('NaN')
 
 
-def row_to_geojson(x, aggr_by, group_by, value, filled, opacity):
+def row_to_geojson(x, aggr_by, group_by, value,
+                   filled, opacity, cumulative, registry):
+    long = x['centroid_longitude']
+    lat = x['centroid_latitude']
+    shapeId = x['shapeID']
+    shapeGrp = x['shapeGroup']
+    val = (x[value['name']] - value['min']) / (value['max'] - value['min'])
+    idxVal = dt.datetime.strftime(x[aggr_by], "%Y") if not isinstance(
+        x[aggr_by], type(pd.NaT)) else float('NaN')
+    if (long, lat, shapeId, shapeGrp) in registry.keys():
+        logger.debug('Found duplicate: longitude= ' + str(long) + ', lat= '
+                     + str(lat) + ', value= ' + str(val))
+        prev = registry[(long, lat, shapeId, shapeGrp)]
+        last_idx = int(prev['properties']['indexValue'][-1])
+        for i in range(last_idx + 1, int(idxVal)):
+            logger.debug('Add index= ' + str(i) + ' for ' + str(long) + ', '
+                         + str(lat))
+            prev['properties']['indexValue'].append(str(i))
+        prev['properties']['transient'] = True
     feature = {'type': 'Feature', 'properties': {}}
-    feature['properties'][ 'opacity' ] = opacity
-    feature['properties'][ 'indexValue' ] = dt.datetime.strftime( x[ aggr_by ], "%Y") if not isinstance(x[ aggr_by ], type(pd.NaT)) else float('NaN')
-    feature['properties'][ 'group' ] = x[ group_by ]
-    feature['properties'][ 'filled' ] = x[ filled['by'] ] == filled['value']
-    feature['properties'][ 'value' ] = ( x[ value['name'] ] - value['min'] ) / ( value['max'] - value['min'] )
+    feature['properties']['opacity'] = opacity
+    feature['properties']['indexValue'] = [idxVal]
+    feature['properties']['group'] = x[group_by]
+    feature['properties']['transient'] = True if not cumulative else False
+    feature['properties']['filled'] = x[filled['by']] == filled['value']
+    feature['properties']['value'] = val
     feature['geometry'] = {
         'type': 'Point',
         'coordinates': [
-            x['centroid_longitude'],
-            x['centroid_latitude'],
+            long, lat,
         ],
     }
+    registry[(long, lat, shapeId, shapeGrp)] = feature
 
     return feature
 
@@ -317,45 +349,56 @@ def default_json(o):
 
 def main(argv):
     parser = argparse.ArgumentParser(
-        description = """
+        description="""
         Tools to aggregate final_df.csv data by recipient country.
 
         input file example: output_data/2.0release/results/2021_09_29_12_06/final_df.csv
         """
     )
     parser.add_argument("input", type=str, help="input file")
-    parser.add_argument("--adm-loc-files", "-a", dest="adm_loc_files", action="store", nargs="+",
+    parser.add_argument("--cumulative", "-c", dest="cumulative",
+                        action="store_true",
+                        help="define if cumsum aggregate values")
+    parser.add_argument("--projects", "-p", dest="projects",
+                        action="store_true",
+                        help="define if add geojson data of each project")
+    parser.add_argument("--adm-loc-files", "-a", dest="adm_loc_files",
+                        action="store", nargs="+",
                         required=True, type=str, help="adm location files")
     parser.add_argument("--aggr-by", "-m", dest="aggr_by", action="store",
                         default="Commitment Year",
-                        required=False, type=str, help="column to aggregate data for index")
+                        required=False, type=str,
+                        help="column to aggregate data for index")
     parser.add_argument("--group-by", "-g", dest="group_by", action="store",
-                        default="Flow Type",
-                        required=False, type=str, help="column to categorize data")
+                        default="Flow Type", required=False,
+                        type=str, help="column to categorize data")
     parser.add_argument("--fill-by", "-f", dest="fill_by", action="store",
                         nargs=2, default=["Status", "Completion"],
-                        required=False, type=str, help="column that defined if data geojson point should be filled "
+                        required=False, type=str, help="column that defined "
+                        "if data geojson point should be filled "
                         "(default:  'Status' 'Completion')")
-    parser.add_argument("--value-column", "-v", dest="value_column", action="store",
-                        default=GEOPANDAS_VALUE_COLUMN,
-                        required=False, type=str, help="Metric to index aggregated data")
+    parser.add_argument("--value-column", "-v", dest="value_column",
+                        action="store", default=GEOPANDAS_VALUE_COLUMN,
+                        required=False, type=str,
+                        help="Metric to index aggregated data")
     parser.add_argument("--sheet", "-s", dest="sheet", action="store",
                         default="GCDF_3.0",
                         required=False, type=str, help="excel sheet")
     parser.add_argument("--output", "-o", dest="output", action="store",
                         required=False, type=str, help="output filename")
-    parser.add_argument("--iso-3166", "-i", dest="isoa3db", action="store", default="iso_a3.json",
-                        required=False, type=str, help="iso alpha-3 country code database json file")
+    parser.add_argument("--iso-3166", "-i", dest="isoa3db", action="store",
+                        default="iso_a3.json", required=False, type=str,
+                        help="iso alpha-3 country code database json file")
     parser.add_argument("--loglevel", "-l", dest="loglevel", action="store",
                         default="info", type=str,
                         choices=loglevel_defs.keys(), help="log level")
-    #parser.add_argument("--quiet", "-q", dest="quiet", action="store_true",
-    #                    help="quiet execution")
+    # parser.add_argument("--quiet", "-q", dest="quiet", action="store_true",
+    #                     help="quiet execution")
 
     args = parser.parse_args(argv)
 
     loglevel = to_loglevel(args.loglevel)
-    # logging.basicConfig(level=logging.NOTSET)
+    logging.basicConfig(level=logging.NOTSET)
     logger.setLevel(loglevel)
 
     input_ = args.input
@@ -364,8 +407,8 @@ def main(argv):
 
     aggr_by = column_name_parser(args.aggr_by)
     group_by = column_name_parser(args.group_by)
-    fill_by = { "by": column_name_parser(args.fill_by[0]),
-                "value": args.fill_by[1] }
+    fill_by = {"by": column_name_parser(args.fill_by[0]),
+               "value": args.fill_by[1]}
     value_column = column_name_parser(args.value_column)
 
     countrycode = CountryCode(isoa3db)
@@ -388,38 +431,25 @@ def main(argv):
     df = pd.read_excel(
         input_, sheet_name=args.sheet,
         dtype=gcdf30_headers, converters=gcdf30_converters,
-        parse_dates=gcdf30_datetime_columns, date_parser=date_parser
+        # parse_dates=gcdf30_datetime_columns, date_parser=date_parser
     )
 
     df = df.rename(columns=column_name_parser)
 
-    geojson_dfs = []
-    for __loc_filename in tqdm(args.adm_loc_files, desc="Processing ADM location files", leave=False):
-        geojson_df = pd.read_csv(__loc_filename, dtype=gcdf30_adm_headers,
-                                 converters=gcdf30_converters)
-        geojson_df = geojson_df.merge(df, left_on="id", right_on="aiddata_record_id")
-        geojson_df = geojson_df[ geojson_df[value_column].isna() == False ]
-        geojson_value = {
-            "name": value_column,
-            "min": geojson_df[ value_column ].min(),
-            "max": geojson_df[ value_column ].max(),
-        }
-        geojson_df["feature"] = geojson_df.apply(lambda x: row_to_geojson(x, aggr_by, group_by,
-                                                                          geojson_value, fill_by,
-                                                                          0.33), axis=1)
-        geojson_dfs.append( geojson_df )
-
-    df = df[ df["recipient_iso_3"].notnull() ]
+    df = df[df["recipient_iso_3"].notnull()]
     df["recipient_code"] = df['recipient'].map(countrycode.reverse_lookup)
     df["recipient_code"].astype("category")
 
-    #df = df[df["Recipient"].isna() == False]
+    # df = df[df["Recipient"].isna() == False]
     df = df.sort_values(["recipient_code", aggr_by], ascending=[True, True])
 
-    new_g = df.groupby(by=["recipient_code", aggr_by], as_index=False)[value_column].sum()
+    new_g = df.groupby(by=["recipient_code", aggr_by],
+                       as_index=False)[value_column].sum()
     new_g[aggr_by] = new_g[aggr_by].map(lambda x: dt.datetime.strftime(x, "%Y"))
-    new_g = new_g.pivot(columns="recipient_code", index=aggr_by, values=value_column).fillna(0)
-    new_g = new_g.cumsum(axis=0)
+    new_g = new_g.pivot(columns="recipient_code", index=aggr_by,
+                        values=value_column).fillna(0)
+    if args.cumulative:
+        new_g = new_g.cumsum(axis=0)
     new_g_dict = new_g.to_dict(orient='split')
     new_g_dict["key"] = new_g_dict["columns"]
     del new_g_dict["columns"]
@@ -427,18 +457,39 @@ def main(argv):
 
     dataout["dataset"]["label"] = {"data": "Total of " + args.value_column}
 
-    # gdf = import_geojson(args.geojson, aggr_by)
-    # gdf["country"] = gdf['country'].map(to_country)
-    # gdf["country3ISO"] = gdf['country'].map(countrycode.reverse_lookup)
-    # gdf["country3ISO"].astype("category")
-    # gdf = gdf.loc[gdf[aggr_by].notnull()]
-    # gdf = gdf.replace([float('NaN')], [None])
-    #dataout["geojson"] = df_to_geojson(gdf)
+    if args.projects:
+        geojson_dfs = []
+        for __loc_filename in tqdm(args.adm_loc_files,
+                                   desc="Processing ADM location files",
+                                   leave=False):
+            geojson_df = pd.read_csv(__loc_filename, dtype=gcdf30_adm_headers,
+                                     converters=gcdf30_converters)
+            geojson_df = geojson_df.merge(df, left_on="id",
+                                          right_on="aiddata_record_id")
+            geojson_df = geojson_df[geojson_df[value_column].isna() == False]
+            geojson_df = geojson_df.sort_values([aggr_by], ascending=[True])
+            if args.cumulative:
+                geojson_df[value_column] = geojson_df.groupby(
+                    by=['centroid_longitude', 'centroid_latitude',
+                        'shapeID', 'shapeGroup'],
+                    as_index=False)[value_column].cumsum(numeric_only=True)
+            geojson_value = {
+                "name": value_column,
+                "min": geojson_df[value_column].min(),
+                "max": geojson_df[value_column].max(),
+            }
+            registry = {}
+            geojson_df["feature"] = geojson_df.apply(
+                lambda x: row_to_geojson(x, aggr_by, group_by, geojson_value,
+                                         fill_by, 0.33, args.cumulative,
+                                         registry),
+                axis=1)
+        geojson_dfs.append(geojson_df)
 
-    dataout["geojson"] = { "type": "FeatureCollection",
-                        "features": [] }
-    for geojson in geojson_dfs:
-        dataout["geojson"]["features"].extend( geojson["feature"].tolist() )
+        dataout["geojson"] = {"type": "FeatureCollection",
+                              "features": []}
+        for geojson in geojson_dfs:
+            dataout["geojson"]["features"].extend(geojson["feature"].tolist())
 
     if output_:
         with open(output_, 'w') as out:
